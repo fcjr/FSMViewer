fa1180ddf3a38da8ed7424ca16e361c23e9f0ed2
Frank Chiarulli Jr <frank@frankchiarulli.com>
2015-02-11 22:12:28 -0500

    adds test suite

---
+++ src/test/FSMSuite.java
@@ -0,0 +1,15 @@
+package edu.union.fsm;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import edu.union.adt.graph.tests.chiarulf.*;
+
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses
+({
+    FSMStoreTests.class
+})
+public class FSMSuite
+{ // no implementation needed; above annotations do the work.
+}
