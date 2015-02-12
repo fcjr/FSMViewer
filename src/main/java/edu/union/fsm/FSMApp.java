87c66ea864efd90dd3a7ff4891e12d57be9f049a
Frank Chiarulli Jr <frank@frankchiarulli.com>
2015-02-12 02:39:02 -0500

    adds the main app file

--- src/main/java/edu/union/fsm/FSMApp.java
+++ src/main/java/edu/union/fsm/FSMApp.java
@@ -1,27 +1,23 @@
+/**
+* Main Application for viewing finite state machines.
+*
+* @author Frank, Rudy, & Nate
+* @version 1
+*/
+
 package edu.union.fsm;
 
-import javax.swing.JFrame;
-
-public Class FSMApp {
+public class FSMApp {
 
     public static void main(String[] args) {
-        EventQueue.invokeLater(new Runnable() {
-            @Override
-            public void run() {
-                FSMApp instance = new FSMApp();
-                instance.start();
-            }
-        });
+
+      View theView = new View();
+
+      Model theModel = new Model();
+
+      Controller theController = new Controller(theView,theModel);
+
+        theView.setVisible(true);
+
     }
-
-    public void start() {
-        JFrame frame = new JFrame("Finite State Machine Viewer");
-        FSMApp app = new FSMApp();
-
-        View viewPane = new View();
-        Control appControl = new Control(app, view);
-
-        frame.getContentPane().add(view);
-        frame.pack();
-        frame.setVisible(true);
 }
