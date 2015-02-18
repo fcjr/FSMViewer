fb9003db70730937a1f4c704cb95e6ad9797e136
Frank Chiarulli Jr <frank@frankchiarulli.com>
2015-02-18 18:02:00 -0500

    updates model to hold the info it needs

--- src/main/java/edu/union/fsm/Model.java
+++ src/main/java/edu/union/fsm/Model.java
@@ -11,6 +11,14 @@ package edu.union.fsm;
 
 public class Model {
 
+public FSMStore fsmStore;
+public DisplayStore displayStore;
 
+
+public Model(){
+    fsmStore = new FSMStore();
+    displayStore = new DisplayStore(8,8);
+
+    }
 
 }
