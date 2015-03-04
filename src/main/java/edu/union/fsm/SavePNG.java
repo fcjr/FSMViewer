06b53970f37a0bcc0ed1350ae1fc4c69b5dbb1bf
Frank Chiarulli Jr <frank@frankchiarulli.com>
2015-03-03 21:00:20 -0500

    now works

--- src/main/java/edu/union/fsm/SavePNG.java
+++ src/main/java/edu/union/fsm/SavePNG.java
@@ -15,6 +15,11 @@ import javax.imageio.ImageIO;
 import javax.swing.JFileChooser;
 import java.awt.image.BufferedImage;
 
+import org.apache.commons.io.FilenameUtils;
+import javax.swing.filechooser.FileFilter;
+import javax.swing.filechooser.FileNameExtensionFilter;
+
+
 public class SavePNG {
 
     private JComponent componentToSave;
@@ -29,21 +34,34 @@ public class SavePNG {
         try {
 
             JFileChooser c = new JFileChooser();
+
+            FileFilter filter = new FileNameExtensionFilter("PNG Files","png");
+            c.setFileFilter(filter);
+
             int returnVal = c.showSaveDialog(paneToPrompt);
 
             if (returnVal == JFileChooser.APPROVE_OPTION) {
 
                 File file = c.getSelectedFile();
 
+                //ENSURE PNG FILE
+                if (FilenameUtils.getExtension(file.getName()).equalsIgnoreCase("png")) {
+                        //DO NOTHING FILE IS GOOD
+                } else {
+                    //replace file extension if is wrong
+                    file = new File(file.getParentFile(), FilenameUtils.getBaseName(file.getName())+".png");
+                }
+
                 Rectangle rect = componentToSave.getVisibleRect();
 
                 BufferedImage image = new BufferedImage(rect.width,rect.height,BufferedImage.TYPE_INT_RGB);
 
-                Graphics graphics = image.getGraphics();
+                Graphics graphics = image.createGraphics();
 
-                //componentToSave.paintComponent(graphics);
+                componentToSave.printAll(graphics);
 
                 ImageIO.write(image, "png", file);
+
             }
             else if (returnVal == JFileChooser.CANCEL_OPTION) {
                 //DO NOTHING
