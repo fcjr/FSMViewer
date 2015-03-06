24b6af04aff5c7ee8b788725e68dd81342d3c4ba
Frank Chiarulli Jr <frank@frankchiarulli.com>
2015-03-05 23:34:40 -0500

    adds funtionality to the InformationStore for single calls from tools

--- src/main/java/edu/union/fsm/InformationStore.java
+++ src/main/java/edu/union/fsm/InformationStore.java
@@ -13,17 +13,17 @@ import java.io.*;
 
 public class InformationStore implements Serializable  {
 
-public FSMStore fsmStore;
-public DisplayStore displayStore;
+    public FSMStore fsmStore;
+    public DisplayStore displayStore;
 
-/**
- * default constructor.
- */
-public InformationStore(){
-    fsmStore = new FSMStore();
-    displayStore = new DisplayStore(8,8);
+    /**
+    * default constructor.
+    */
+    public InformationStore(){
+        fsmStore = new FSMStore();
+        displayStore = new DisplayStore(8,8);
 
-    }
+        }
 
 public void load(InformationStore modelToLoad, InformationStoreListener l) {
     this.fsmStore = modelToLoad.fsmStore;
@@ -43,5 +43,62 @@ public void cleanForWriting() {
     fsmStore.clearListeners();
 }
 
+public void addState(String name, int firstX,int firstY) throws StoreException {
+    if (!displayStore.containsState(firstX,firstY)){
+        int id = fsmStore.addState(name);
+        displayStore.addState(firstX,firstY,id);
+    } else {
+        throw new StoreException("Can't Add State, state already exists in position.");
+    }
+}
+
+public void addTransition(String name, int firstX, int firstY, int secondX, int secondY) throws StoreException {
+    if(displayStore.containsState(firstX,firstY) &&
+       displayStore.containsState(secondX,secondY)) {
+           int fromID = displayStore.getState(firstX,firstY);
+           int toID = displayStore.getState(secondX,secondY);
+           fsmStore.addTransition(name,fromID,toID);
+    } else {
+        throw new StoreException("Error: Not a valid transition.");
+    }
+}
+
+public void removeState(int firstX,int firstY) throws StoreException{
+    if(displayStore.containsState(firstX,firstY)){
+        int id = displayStore.removeState(firstX,firstY);
+        fsmStore.removeState(id);
+    }
+}
+
+public void removeTransition(int firstX, int firstY, int secondX, int secondY) throws StoreException{
+    if(displayStore.containsState(firstX,firstY) &&
+       displayStore.containsState(secondX,secondY)) {
+           int fromID = displayStore.getState(firstX,firstY);
+           int toID = displayStore.getState(secondX,secondY);
+              if (fsmStore.containsTransitionWithIDs(fromID,toID)){
+                  fsmStore.removeTransitionWithIDs(fromID,toID);
+              } else{
+                  throw new StoreException("Error: No transtion exists.");
+              }
+       } else {
+           throw new StoreException("Error: Invalid States.");
+       }
+}
+
+public void moveState(int firstX, int firstY, int secondX, int secondY) throws StoreException {
+    if(displayStore.containsState(firstX,firstY) &&
+       !displayStore.containsState(secondX,secondY)) {
+           displayStore.moveState(firstX,firstY,secondX,secondY);
+       } else{
+           throw new StoreException("Error: Not a valid move.");
+       }
+}
+
+public void toggleStateType(int firstX, int firstY) throws StoreException {
+    if(displayStore.containsState(firstX,firstY)){
+        int id = displayStore.getState(firstX,firstY);
+        fsmStore.toggleStateType(id);
+    }
+}
 
 }
