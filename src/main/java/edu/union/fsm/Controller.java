a029564836c27ba305725d2607d496528d9505ee
Frank Chiarulli Jr <frank@frankchiarulli.com>
2015-03-07 17:30:44 -0500

    implements ToolSetter and cleans up constructor

--- src/main/java/edu/union/fsm/Controller.java
+++ src/main/java/edu/union/fsm/Controller.java
@@ -7,8 +7,9 @@
 
 package edu.union.fsm;
 
-import edu.union.fsm.tool.*;
+import edu.union.fsm.tools.*;
 import edu.union.fsm.saving.*;
+
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
@@ -18,246 +19,189 @@ import java.awt.event.MouseListener;
 
 public class Controller implements MouseListener {
 
-  private SwingDisplay swingDisplay;
-  private InformationStore informationStore;
-  private Tool currentTool;
-  private ToolInfoHolder toolInfoHolder;
+    private static final boolean debug = false;
 
-  public Controller(SwingDisplay swingDisplay, InformationStore informationStore) {
-    this.swingDisplay = swingDisplay;
-    this.informationStore = informationStore;
-    informationStore.addListener(swingDisplay);
+    private SwingDisplay swingDisplay;
+    private InformationStore informationStore;
+    private Tool currentTool;
+    private ToolInfoHolder toolInfoHolder;
 
+    public Controller(SwingDisplay swingDisplay, InformationStore informationStore) {
+        this.swingDisplay = swingDisplay;
+        this.informationStore = informationStore;
+        informationStore.addListener(swingDisplay);
 
-    toolInfoHolder = new ToolInfoHolder(informationStore, swingDisplay);
-    currentTool = new DefaultTool(toolInfoHolder);
-
-
-    //Adds all of the control listeners to their respected Jpane
-    this.swingDisplay.addAddStateButtonListener(new AddStateListener());
-    this.swingDisplay.addDeleteStateButtonListener(new DeleteStateListener());
-    this.swingDisplay.addMoveStateButtonListener(new MoveStateListener());
-    this.swingDisplay.addToggleTypeButtonListener(new ToggleTypeButtonListener());
-    this.swingDisplay.addAddTransitionButtonListener(new AddTransitionButtonListener());
-    this.swingDisplay.addDeleteTransitionButtonListener(new DeleteTransitionButtonListener());
-    this.swingDisplay.addSaveBinButtonListener(new SaveBinButtonListener());
-    this.swingDisplay.addLoadBinButtonListener(new LoadBinButtonListener());
-    this.swingDisplay.addSavePNGButtonListener(new SavePNGButtonListener());
-    this.swingDisplay.addMouseListener(this);
-  }
-
-  //MOUSE EVENTS
-  public void mouseExited(MouseEvent e) {}
-  public void mouseClicked(MouseEvent e) {}
-  public void mouseEntered(MouseEvent e) {}
-
-  public void mousePressed(MouseEvent e) {
-      int firstX = swingDisplay.coordToCellSpot(e.getX());
-      toolInfoHolder.setFirstX(firstX);
-
-      int firstY = swingDisplay.coordToCellSpot(e.getY());
-      toolInfoHolder.setFirstY(firstY);
-  }
-  public void mouseReleased(MouseEvent e) {
-      int secondX = swingDisplay.coordToCellSpot(e.getX());
-      toolInfoHolder.setSecondX(secondX);
-
-      int secondY = swingDisplay.coordToCellSpot(e.getY());
-      toolInfoHolder.setSecondY(secondY);
-
-      try {
-          currentTool.execute();
-      } catch (ToolException ex) {
-          String toPrint = ex.getMessage();
-          swingDisplay.displayErrorMessage(toPrint);
-      }
-  }
+        //Setup tool info and default tool.
+        toolInfoHolder = new ToolInfoHolder(informationStore, swingDisplay);
+        currentTool = new DefaultTool(toolInfoHolder);
 
 
-  //BUTTON LISTENERS all these do are toggle the current tool.
-  private class AddStateListener implements ActionListener{
+        //Adds all of the control listeners to their respected Jpane
+        populateDisplayListeners();
 
-    public void actionPerformed(ActionEvent e) {
-
-      try{
-
-         currentTool = new AddStateTool(toolInfoHolder);
-      }
-
-      catch(Exception ex){
-
-        System.out.println(ex);
-
-        swingDisplay.displayErrorMessage("Error Selecting Tool");
-
-      }
-
+        this.swingDisplay.addMouseListener(this);
     }
 
-  }
+    //MOUSE EVENTS
+    public void mouseExited(MouseEvent e) {}
+    public void mouseClicked(MouseEvent e) {}
+    public void mouseEntered(MouseEvent e) {}
 
-  private class DeleteStateListener implements ActionListener{
 
-    public void actionPerformed(ActionEvent e) {
+    public void mousePressed(MouseEvent e) {
+        int firstX = swingDisplay.coordToCellSpot(e.getX());
+        toolInfoHolder.setFirstX(firstX);
 
-      try{
+        int firstY = swingDisplay.coordToCellSpot(e.getY());
+        toolInfoHolder.setFirstY(firstY);
+    }
+    public void mouseReleased(MouseEvent e) {
+        int secondX = swingDisplay.coordToCellSpot(e.getX());
+        toolInfoHolder.setSecondX(secondX);
 
-        currentTool = new DeleteStateTool(toolInfoHolder);
-      }
+        int secondY = swingDisplay.coordToCellSpot(e.getY());
+        toolInfoHolder.setSecondY(secondY);
 
-      catch(Exception ex){
-
-        System.out.println(ex);
-
-        swingDisplay.displayErrorMessage("Error Selecting Tool");
-
-      }
-
+        executeCurrentTool();
     }
 
-  }
+    //Other Functions
 
-  private class MoveStateListener implements ActionListener{
+    /**
+     * Sets all of the Button Liseners in the SwingDisplay to their poper ToolSetters.
+     */
+    private void populateDisplayListeners() {
 
-    public void actionPerformed(ActionEvent e) {
+        //Add State Button
+        Tool tool = new AddStateTool(toolInfoHolder);
+        ToolSetter setter = new ToolSetter(tool, this);
+        this.swingDisplay.addAddStateButtonListener(setter);
 
-      try{
+        //Delete State Button
+        tool = new DeleteStateTool(toolInfoHolder);
+        setter = new ToolSetter(tool, this);
+        this.swingDisplay.addDeleteStateButtonListener(setter);
 
-        currentTool = new MoveStateTool(toolInfoHolder);
-      }
+        //Move State Button
+        tool = new MoveStateTool(toolInfoHolder);
+        setter = new ToolSetter(tool, this);
+        this.swingDisplay.addMoveStateButtonListener(setter);
 
-      catch(Exception ex){
+        //Toggle Type Button
+        tool = new ToggleTypeTool(toolInfoHolder);
+        setter = new ToolSetter(tool, this);
+        this.swingDisplay.addToggleTypeButtonListener(setter);
 
-        System.out.println(ex);
+        //Add Transition Button
+        tool = new AddTransitionTool(toolInfoHolder);
+        setter = new ToolSetter(tool, this);
+        this.swingDisplay.addAddTransitionButtonListener(setter);
 
-        swingDisplay.displayErrorMessage("Error Selecting Tool");
+        //Delete Transition Button
+        tool = new DeleteTransitionTool(toolInfoHolder);
+        setter = new ToolSetter(tool, this);
+        this.swingDisplay.addDeleteTransitionButtonListener(setter);
 
-      }
-
+        //NON TOOL LISTENERS
+        this.swingDisplay.addSaveBinButtonListener(new SaveBinButtonListener());
+        this.swingDisplay.addLoadBinButtonListener(new LoadBinButtonListener());
+        this.swingDisplay.addSavePNGButtonListener(new SavePNGButtonListener());
     }
 
-  }
-
-  private class ToggleTypeButtonListener implements ActionListener{
-
-    public void actionPerformed(ActionEvent e) {
-
-      try{
-
-        currentTool = new ToggleTypeTool(toolInfoHolder);
-      }
-
-      catch(Exception ex){
-
-        System.out.println(ex);
-
-        swingDisplay.displayErrorMessage("Error Selecting Tool");
-
-      }
-
+    /**
+    * Executes the current Tool.
+    */
+    private void executeCurrentTool() {
+        try {
+            currentTool.execute();
+        } catch (ToolException ex) {
+            if(debug) {
+                System.out.println(ex);
+                ex.printStackTrace();
+            }
+            String toPrint = ex.getMessage();
+            swingDisplay.displayErrorMessage(toPrint);
+        }
     }
 
-  }
-
-  private class AddTransitionButtonListener implements ActionListener{
-
-    public void actionPerformed(ActionEvent e) {
-
-      try{
-
-        currentTool = new AddTransitionTool(toolInfoHolder);
-      }
-
-      catch(Exception ex){
-
-        System.out.println(ex);
-
-        swingDisplay.displayErrorMessage("Error Selecting Tool");
-
-      }
-
+    /**
+    * Setts the currentTool of the Controller.
+    * @param toolToSet the tool to set.
+    */
+    protected void setCurrentTool(Tool toolToSet) {
+        try {
+            currentTool = toolToSet;
+        } catch (Exception ex) {
+            if(debug) {
+                System.out.println(ex);
+                ex.printStackTrace();
+            }
+            swingDisplay.displayErrorMessage("Error Selecting Tool. Please Try again.");
+        }
     }
 
-  }
 
-  private class DeleteTransitionButtonListener implements ActionListener{
+    //BUTTON LISTENERS all these do are toggle the current tool.
+    private class SaveBinButtonListener implements ActionListener{
 
-    public void actionPerformed(ActionEvent e) {
+        public void actionPerformed(ActionEvent e) {
 
-      try{
+            try{
 
-          currentTool = new DeleteTransitionTool(toolInfoHolder);
-      }
+                SaveBin saver = new SaveBin(informationStore, swingDisplay);
+                saver.saveFile();
+                informationStore.addListener(swingDisplay);
+            }
 
-      catch(Exception ex){
+            catch(Exception ex){
 
-        System.out.println(ex);
+                ex.printStackTrace();
+                System.out.println(ex);
 
-        swingDisplay.displayErrorMessage("Error Selecting Tool");
-      }
+                swingDisplay.displayErrorMessage("Error Selecting Tool");
+            }
+        }
     }
-  }
 
-  private class SaveBinButtonListener implements ActionListener{
+    private class LoadBinButtonListener implements ActionListener{
 
-    public void actionPerformed(ActionEvent e) {
+        public void actionPerformed(ActionEvent e) {
 
-      try{
+            try {
 
-          SaveBin saver = new SaveBin(informationStore, swingDisplay);
-          saver.saveFile();
-          informationStore.addListener(swingDisplay);
-      }
+                LoadBin loader = new LoadBin(swingDisplay);
+                Object toLoad = loader.loadFile();
+                InformationStore loadInformationStore = (InformationStore) toLoad;
+                informationStore.load(loadInformationStore, swingDisplay);
 
-      catch(Exception ex){
+            } catch(NullPointerException ex) {
+                //DO NOTHING
+            } catch(Exception ex){
 
-         ex.printStackTrace();
-        System.out.println(ex);
+                System.out.println(ex);
+                ex.printStackTrace();
 
-        swingDisplay.displayErrorMessage("Error Selecting Tool");
-      }
+                swingDisplay.displayErrorMessage("Error Selecting Tool");
+            }
+        }
     }
-  }
 
-  private class LoadBinButtonListener implements ActionListener{
+    private class SavePNGButtonListener implements ActionListener{
 
-    public void actionPerformed(ActionEvent e) {
+        public void actionPerformed(ActionEvent e) {
 
-      try {
+            try {
+                SavePNG saver = new SavePNG(swingDisplay.getMainDisplayComponent(), swingDisplay);
+                saver.saveFile();
+            } catch(Exception ex){
 
-          LoadBin loader = new LoadBin(swingDisplay);
-          Object toLoad = loader.loadFile();
-          InformationStore loadInformationStore = (InformationStore) toLoad;
-          informationStore.load(loadInformationStore, swingDisplay);
+                System.out.println(ex);
+                ex.printStackTrace();
 
-      } catch(NullPointerException ex) {
-        //DO NOTHING
-      } catch(Exception ex){
-
-        System.out.println(ex);
-        ex.printStackTrace();
-
-        swingDisplay.displayErrorMessage("Error Selecting Tool");
-      }
+                swingDisplay.displayErrorMessage("Error Selecting Tool");
+            }
+        }
     }
-  }
-
-  private class SavePNGButtonListener implements ActionListener{
-
-    public void actionPerformed(ActionEvent e) {
-
-      try {
-          SavePNG saver = new SavePNG(swingDisplay.getMainDisplayComponent(), swingDisplay);
-          saver.saveFile();
-      } catch(Exception ex){
-
-        System.out.println(ex);
-        ex.printStackTrace();
-
-        swingDisplay.displayErrorMessage("Error Selecting Tool");
-      }
-    }
-  }
 
 
 }
