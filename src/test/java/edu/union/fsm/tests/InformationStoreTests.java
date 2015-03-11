605e1bc8d30c6d7ba8d0691f129384c398cbb257
Rodolfo Santana <rodolfosantana7@gmail.com>
2015-03-11 10:53:18 -0400

    Wrote tests for move state, all passing

--- src/test/java/edu/union/fsm/tests/InformationStoreTests.java
+++ src/test/java/edu/union/fsm/tests/InformationStoreTests.java
@@ -41,15 +41,19 @@ public class InformationStoreTests {
     public void testAddState() {
 
 	try{
+
 	    assertFalse("There should be no state to begin with.", informationStore.containsState(1,1));
-	
+
+	    // add state in empty cell
 	    informationStore.addState("Hello", 1, 1);	
 	    assertTrue("There should exist a state at location(1,1).", informationStore.containsState(1,1));
-	
+	    
 	    State tempState = informationStore.getState(1,1);
 	    String tempName = tempState.getName();
 	    assertEquals("Name should be Hello", "Hello", tempName);
 
+
+	    // adding state in cell with already existing state in it
 	    informationStore.addState("Bye", 1, 1);
 	    State toCheck = informationStore.getState(1,1);
 	    String name = toCheck.getName();
@@ -82,15 +86,19 @@ public class InformationStoreTests {
     @Test
     public void testRemoveState() {
 	try{
-	    assertFalse("There should be no state to begin with.", informationStore.containsState(1,1));
-	
-	    informationStore.addState("Hello", 1, 1);
-	    informationStore.addState("Bye", 2, 2);
-	    assertTrue("There should exist a state at location(1,1).", informationStore.containsState(1,1));
-	    assertTrue("There should exist a state at location(2,2).", informationStore.containsState(2,2));
 
-	   informationStore.removeState(1, 1);
+	    // remove existing state
+	    informationStore.addState("Hello", 1, 1);
+	    assertTrue("There should exist a state at location(1,1).", informationStore.containsState(1,1));
+
+	    informationStore.removeState(1, 1);
 	    assertFalse("There should no longer be a state at position (1,1)", informationStore.containsState(1,1));
+
+
+	    // remove non-existing state
+	    informationStore.removeState(4, 4);
+	    assertFalse("Removing nonexistant state should not break it.", informationStore.containsState(4,4));
+	    
 	} catch (Exception ex) {}
 	
     }
@@ -102,23 +110,60 @@ public class InformationStoreTests {
 
     @Test
     public void testMoveState() {
+	try{
 
+	    // moving existant state
+	    informationStore.addState("Hello", 1, 1);
+	    assertTrue("There should exist a state at location(1,1).", informationStore.containsState(1,1));
+
+	    informationStore.moveState(1, 1, 7, 7);
+	    assertFalse("There should no longer exist a state at location(1,1).", informationStore.containsState(1,1));
+	    assertTrue("There should exist a state at location(7,7).", informationStore.containsState(7,7));
+
+	    State tempState = informationStore.getState(7,7);
+	    String tempName = tempState.getName();
+	    assertEquals("Name should be Hello", "Hello", tempName);
+
+
+	    // moving state more than once
+	    informationStore.moveState(7, 7, 5, 2);
+	    assertFalse("There should no longer exist a state at location(7,7).", informationStore.containsState(7,7));
+	    assertTrue("There should exist a state at location(5,2).", informationStore.containsState(5,2));
+
+	    State tempState2 = informationStore.getState(5,2);
+	    String tempName2 = tempState.getName();
+	    assertEquals("Name should be Hello", "Hello", tempName2);
+
+
+	    // moving state to non-empty cell
+	    informationStore.addState("World", 7, 5);
+	    informationStore.moveState(5, 2, 7, 5);
+	    
+	    State tempState3 = informationStore.getState(7,5);
+	    String tempName3 = tempState.getName();
+	    assertEquals("Name should not change", "World", tempName3);
+	    assertTrue("There should still exist a state at location(5,2).", informationStore.containsState(5,2));
+	    assertTrue("There should still exist a state at location(7,5).", informationStore.containsState(7,5));
+
+	    
+	    // moving non-existant state
+	    informationStore.moveState(3, 4, 6, 6);
+	    assertFalse("There should be no state at location(6,6). This should not break.", informationStore.containsState(6,6));
+		    
+	} catch (Exception ex) {}
+	
     }
 
-    @Test
-    public void testGetStart() {
-
-    }
     
-    @Test
-    public void testGetRow() {
+//  @Test
+//  public void testGetRow() {
+//	
+//  }
 
-    }
-
-    @Test
-    public void testGetColumn() {
-
-    }
+//  @Test
+//  public void testGetColumn() {
+//	
+//  }
 
     
 }
